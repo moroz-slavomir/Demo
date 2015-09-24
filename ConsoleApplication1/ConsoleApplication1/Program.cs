cd0e3535fba7d985ca1b69033fcdbc38dd6bcf07
Slavomir Moroz <selepko121@gmail.com>
2015-09-24 09:40:18 +0200

    Second

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -1,13 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ConsoleApplication1
+﻿namespace ConsoleApplication1
 {
     class Program
     {
+        // Comment
         static void Main(string[] args)
         {
         }
